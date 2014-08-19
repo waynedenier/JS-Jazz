d8d4b176767dcfa22b7a5673dbed9d7cd96fa43b
Wayne Denier <wayne@headspring.com>
2014-08-19 11:40:32 -0400

    Adding Angular.js

--- src/App_Start/BundleConfig.cs
+++ src/App_Start/BundleConfig.cs
@@ -10,8 +10,8 @@ namespace JS_Jazz
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //    "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/angular.js"));
 
             //bundles.Add(new StyleBundle("~/Content/css").Include(
             //     "~/Content/bootstrap.css",
